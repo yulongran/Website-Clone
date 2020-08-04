e54c395222642f1bfa432faafdcf83b4641db14f
Yulong Ran <yulongran@Yulongs-MacBook-Air.local>
2020-08-04 13:54:24 -0700

    Update header css to make it responsive

--- youtube-clone/src/components/Header/Header.js
+++ youtube-clone/src/components/Header/Header.js
@@ -12,20 +12,29 @@ function Header() {
   return (
     <div className="header">
       <div className="header-left">
-        <DehazeOutlined />
+        <button className="icon-button">
+          <DehazeOutlined className="back-icon" />
+        </button>
         <img
           className="youtube-img"
+          alt=""
           src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b8/YouTube_Logo_2017.svg/320px-YouTube_Logo_2017.svg.png"
         />
       </div>
       <div className="header-search">
         <div className="search-wrapper">
-          <div className="search-container">
-            <input className="search-input" type="text" placeholder="Search" />
-          </div>
-          <button className="search-button">
-            <SearchOutlined className="search-icon" fontSize="x-small" />
-          </button>
+          <form className="search-form">
+            <div className="search-container">
+              <input
+                className="search-input"
+                type="text"
+                placeholder="Search"
+              />
+            </div>
+            <button className="search-button">
+              <SearchOutlined className="search-icon" fontSize="x-small" />
+            </button>
+          </form>
         </div>
       </div>
       <div className="header-right">
@@ -34,6 +43,7 @@ function Header() {
         <Notifications className="header-right-icon" />
         <img
           className="profile-image"
+          alt=""
           src="https://bjedrocha.com/images/posts/gmail-style-avatars/avatar.png"
         />
       </div>
